02beb8d35db926ca52fbffa447d007848653e826
Tove-Larsson <144339361+Tove-Larsson@users.noreply.github.com>
2024-12-10 16:33:23 +0100

    Create Restaurant Entity

--- src/main/java/com/tove/examensarbetebackend/model/Restaurant.java
+++ src/main/java/com/tove/examensarbetebackend/model/Restaurant.java
@@ -1,4 +1,85 @@
 package com.tove.examensarbetebackend.model;
 
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "restaurants")
 public class Restaurant {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String address;
+    private int toilet;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public Restaurant() {}
+
+    public Restaurant(String name, String address, int toilet) {
+        this.name = name;
+        this.address = address;
+        this.toilet = toilet;
+    }
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+        updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getToilet() {
+        return toilet;
+    }
+
+    public void setToilet(int toilet) {
+        this.toilet = toilet;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
 }
