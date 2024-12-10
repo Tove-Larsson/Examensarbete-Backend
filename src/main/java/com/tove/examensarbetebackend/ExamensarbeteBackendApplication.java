6025ff3d6410f8c1a3fde5da9cc09a6adf3c3330
Tove-Larsson <144339361+Tove-Larsson@users.noreply.github.com>
2024-12-10 13:42:12 +0100

    Initial commit

---
+++ src/main/java/com/tove/examensarbetebackend/ExamensarbeteBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tove.examensarbetebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamensarbeteBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamensarbeteBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tove/examensarbetebackend/ExamensarbeteBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tove.examensarbetebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamensarbeteBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
